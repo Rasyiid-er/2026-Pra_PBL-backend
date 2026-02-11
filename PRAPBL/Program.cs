8503ff8bc677256201d2b68dd22c201236a2c656
rasyiid <eraratna92@gmail.com>
2026-02-11 18:18:20 +0700

    feat: configure CORS and add DbContext for Room management

--- PRAPBL/Program.cs
+++ PRAPBL/Program.cs
@@ -5,13 +5,31 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 Env.Load();
 
+
+var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRINGS");
+builder.Services.AddDbContext<PRAPBLContext>(options =>
+    options.UseSqlServer(connectionString!));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 
 var app = builder.Build();
 
+app.UseCors("AllowSpecificOrigins");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -19,6 +37,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
 
